7fe8d2268470a518b946599fa67f67e15fa92135
Tayssir Ayari <61150382+tayssir89@users.noreply.github.com>
2022-08-25 12:06:50 +0200

    Update form-page.php

--- form-page.php
+++ form-page.php
@@ -1,1 +1,25 @@
-
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+ 
+$email_from = '';
+ 
+$email_subject = 'Nouveau message de : ' . $name;
+ 
+$email_body = "Nom du contact : $name .\n".
+                "Adresse Email : $visitor_email .\n".
+                "Sujet : $subject .\n".
+                "Message : $message .\n";
+ 
+$to = 'ayaritayssir8@gmail.com';
+ 
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+ 
+mail($to,$email_subject,$email_body,$headers);
+ 
+header("Location: index.html");
+ 
+?>
